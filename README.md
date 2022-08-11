2598dc7898feeb4f18a4e976d3e42e1665a77afe
altamir95 <altamir95@yandex.ru>
2022-08-11 09:29:25 +0300

    first

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # TestingVault
+
+Для начала щапускаем команду для поддержки vault
+docker pull vault
+
+Создаем контейнер с Vault
+docker run --name vault -p 8200:8200 vault:latest
+Результатом команды в консоли будет выведен следующая команда
+
+You may need to set the following environment variable:
+
+    $ export VAULT_ADDR='http://0.0.0.0:8200'
+
+The unseal key and root token are displayed below in case you want to
+seal/unseal the Vault or re-authenticate.
+
+Unseal Key: TkSC74J0vzClvql9IEUJwdHvtyL+gLybyWWk0DWQ7zU=
+Root Token: hvs.pbqnPGi3Y5I44FK4rlGAZxCF
+
+Development mode should NOT be used in production installations!
+
+Скопируйте токен из RootToken и перенести в appsittegs.json полю vault token
+Так же запустите команду export VAULT_ADDR='http://0.0.0.0:8200' но в терминале контейнера который мы создали ранее
+Так же введите коменлу export VAULT_TOKEN='токен из RootToken' для того чо бы не логиниться в теминаое докер контейнер
+
+Далее в терминал докер крейсера введите команду для создания секретного ключа
+
+Vault kv put secret/tdc tdcpassword=test1234
+
+
---
+++ TestingVault/TestingVault/Controllers/SecretController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.Commons;
+
+namespace TestingVault.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SecretController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SecretController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<Secret<SecretData>> GetAsync()
+        {
+
+            IAuthMethodInfo authMethod = new TokenAuthMethodInfo(_configuration.GetSection("VaultToken").Value);
+
+            var vaultClientSettings = new VaultClientSettings(_configuration.GetSection("VaultUrl").Value, authMethod);
+
+            IVaultClient vaultClient = new VaultClient(vaultClientSettings);
+
+            var kv2Secret = await vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(path: "tdc", mountPoint: "secret");
+
+            return kv2Secret;
+        }
+    }
+}
+
